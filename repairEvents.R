9e9de6b61198c80ffb5bdfe159cfe528a48852e4
Adam Mattsson <mattsada@gmail.com>
2021-02-24 15:45:41 -0800

    file add

--- repairEvents.R
+++ repairEvents.R
@@ -37,7 +37,7 @@ breakpoints$gene<-as.factor(breakpoints$gene)
             # Summary #
 ################################################
 
-summary <- breakpoints %>% group_by(filenames,gene)%>%summarize(n())
+summary <- breaks %>% group_by(filenames,gene)%>%summarize(n())
 summary <- summary %>% group_by(gene)%>%summarize(n())
 
 summaryBreaks.df=GRanges(breakpoints)
@@ -56,8 +56,14 @@ for (row in 1:length(breakGRanges)){
   breakGRanges[row,]$freq = countOverlaps(tmp,query, maxgap = 500000)
 }
 
-breaks = merge(as.data.frame(breakGRanges),summary,by="gene")
+breaks = merge(as.data.frame(breaks),summary,by="gene")
 breaks$alleleFreq=round(((breaks$freq/breaks$`n()`)*100),digits = 0)
+
+breaks <- read.table("breaks.txt",header=T)
+
+library(breakpointR)
+files2plot=paste0("../data/data/",levels(as.factor(test$filenames)))
+plotBreakpointsPerChr(files2plot,plotspath = "PLOTS",chromosomes = c(levels(test$seqnames)))
 
 
 inversions <- breakGRanges[breakGRanges$allele_freq>filterFrequency,]
@@ -72,20 +78,58 @@ recurring$alleleFreq<-as.factor(recurring$alleleFreq)
 
 i=levels(recurring$gene)[1]
 j=levels(tmp$alleleFreq)[1]
-for (i in levels(breaks$gene)){
+
+breaks$gene=str_replace(breaks$gene,"/","-")
+breaks$gene <- as.factor(breaks$gene)
+breaks$alleleFreq<-as.factor(breaks$alleleFreq)
+
+recurring$gene <- as.factor(recurring$gene)
+recurring$alleleFreq<-as.factor(recurring$alleleFreq)
+
+
+t=levels(tmp2$seqnames)[2]
+
+
+breaks
+
+for (i in levels(recurring$gene)){
   tmp <- filter(recurring,gene==i)
   for (j in levels(tmp$alleleFreq)){
     tmp2 <- filter(tmp, alleleFreq==j)
-    vec = tmp2$seqnames
-    chr <- names(table(vec))[as.vector(table(vec))==max(table(vec))]
-    tmp2 <- filter(tmp2,seqnames==chr)
-    
+    tmp2$seqnames<-droplevels(tmp2$seqnames)
+    for (t in levels(tmp2$seqnames)){
+      chr=t
+      tmp3 <- filter(tmp2,seqnames==chr)
+      if (nrow(tmp3) > 5){
+        tmp3$filenames <- droplevels(tmp3$filenames)
+        
+        bed <- tmp3 %>% select(seqnames,start,end,filenames)
+        export(bed,"bed.bed",format = "gff3")
+        
+        plotspath=paste0("PLOTS/",chr,"-",i,"-",j,"-f/")
+        datapath=paste0(plotspath,"data/")
+        
+        if (!file.exists(plotspath) ) { dir.create(plotspath)}
+        if (!file.exists(datapath) ) { dir.create(datapath)}
+        
+        
+        readsFiles<- tools::file_path_sans_ext(levels(tmp3$filenames))
+        files2transfer=paste0("../data/browserfiles/",readsFiles,"_reads.bed.gz")
+        
+        file.copy(files2transfer,datapath)
+        
+        files2plot=paste0("../data/data/",levels(tmp3$filenames))
+        plotBreakpointsPerChr(files2plot,plotspath = plotspath,chromosomes = c(chr))
+      }
+    }
   }
 }
 
 
 
-
+t=chr11
+gene="BLM"
+freq=26
 
 test <-  filter(breaks,gene=="BLM/RECQL5" & alleleFreq >57 & alleleFreq<75)
 
