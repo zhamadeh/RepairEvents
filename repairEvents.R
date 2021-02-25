f11662390be97ee330ea3ba6f2e74779b9cfd8d0
Adam Mattsson <mattsada@gmail.com>
2021-02-25 13:32:59 -0800

    new

--- repairEvents.R
+++ repairEvents.R
@@ -4,6 +4,8 @@
 library(tidyverse)
 library(GenomicRanges)
 library(breakpointR)
+library(rtracklayer)
+library(doParallel)
 
 ################################################
             # Data #
@@ -66,34 +68,88 @@ for (row in 1:length(breakGRanges)){
 }
 
 breaks = merge(as.data.frame(breaks),summary,by="gene")
-breaks$alleleFreq=round(((breaks$freq/breaks$`n()`)*100),digits = 0)
-
-
-#breaks <- read.table("breaks.txt",header=T)
-
-
-
-
-
-################################################
-                  # plotting #
-################################################
-
-inversions <- breakGRanges[breakGRanges$allele_freq>filterFrequency,]
-breaks <- breakGRanges[breakGRanges$allele_freq<filterFrequency,]
-breaks <- as.data.frame(breaks)
-
-breaks <- filter(breaks,width<1000000)
+breaks$alleleFreq=round((breaks$freq/breaks$n..*100),digits = 0)
 
 breaks$gene=str_replace(breaks$gene,"/","-")
 breaks$gene <- as.factor(breaks$gene)
-breaks$alleleFreq<-as.factor(breaks$alleleFreq)
 
 recurring=filter(breaks,alleleFreq > 25)
 recurring$alleleFreq<-as.factor(recurring$alleleFreq)
 
 recurring$gene <- as.factor(recurring$gene)
 recurring$alleleFreq<-as.factor(recurring$alleleFreq)
+
+write.table(breaks,"DATA/breaks.txt",quote = F,row.names = F,col.names = T,sep="\t")
+write.table(recurring,"DATA/recurringBreaksCutoff25.txt",quote = F,row.names = F,col.names = T,sep="\t")
+
+breaks <- read.table("DATA/breaks.txt",header=T)
+breaks <- read.table("DATA/breaks.txt",header=T)
+
+
+################################################
+                  # functions #
+################################################
+
+
+repairER <- function(i, recurring, breaks){
+  tmp <- filter(recurring,gene==i)
+  for (j in levels(tmp$alleleFreq)){
+    tmp2 <- filter(tmp, alleleFreq==j)
+    tmp2$seqnames<-droplevels(tmp2$seqnames)
+    for (t in levels(tmp2$seqnames)){
+      chr=t
+      tmp3 <- filter(tmp2,seqnames==chr)
+      if (nrow(tmp3) > 5){
+        message("Found a possible inversion on ", chr," in ", j,"% of ",i," cells.")
+        
+        tmp3$filenames <- droplevels(tmp3$filenames)
+        
+        plotspath=paste0("PLOTS/",chr,"-",i,"-",j,"-f/")
+        datapath=paste0(plotspath,"data/")
+        
+        if (!file.exists(plotspath) ) { dir.create(plotspath)}
+        if (!file.exists(datapath) ) { dir.create(datapath)}
+        
+        #use tmp2 so you get other breakpoints that were also on the same chromosome
+        tmp4 <- filter(breaks,gene==i)
+        bed <- tmp4 %>% filter(seqnames==chr & filenames %in% tmp3$filenames) %>% select(seqnames,start,end,filenames)
+        bedpath=paste0(plotspath,"breakpoints.bed")
+        export(bed,bedpath,format = "gff3")
+        
+        readsFiles<- tools::file_path_sans_ext(levels(tmp3$filenames))
+        files2transfer=paste0("DATA/browserfiles/",readsFiles,"_reads.bed.gz")
+        
+        file.copy(files2transfer,datapath)
+        
+        files2plot=paste0("DATA/browserfiles/",levels(tmp3$filenames))
+        plotBreakpointsPerChr(files2plot,plotspath = plotspath,chromosomes = c(chr))
+      }
+    }
+  }
+}
+
+startTimedMessage <- function(...) {
+  
+  x <- paste0(..., collapse='')
+  message(x, appendLF=FALSE)
+  ptm <- proc.time()
+  return(ptm)
+  
+}
+
+
+stopTimedMessage <- function(ptm) {
+  
+  time <- proc.time() - ptm
+  message(" ", round(time[3],2), "s")
+  
+}
+
+################################################
+     # Running functions generate output #
+################################################
+
+ptm <- startTimedMessage("Reading file ", bamfile, " ...")
 
 for (i in levels(recurring$gene)){
   tmp <- filter(recurring,gene==i)
@@ -104,10 +160,9 @@ for (i in levels(recurring$gene)){
       chr=t
       tmp3 <- filter(tmp2,seqnames==chr)
       if (nrow(tmp3) > 5){
-        tmp3$filenames <- droplevels(tmp3$filenames)
+        message("Found a possible inversion on ", chr," in ", j,"% of ",i," cells.")
         
-        bed <- tmp3 %>% select(seqnames,start,end,filenames)
-        export(bed,"bed.bed",format = "gff3")
+        tmp3$filenames <- droplevels(tmp3$filenames)
         
         plotspath=paste0("PLOTS/",chr,"-",i,"-",j,"-f/")
         datapath=paste0(plotspath,"data/")
@@ -115,17 +170,37 @@ for (i in levels(recurring$gene)){
         if (!file.exists(plotspath) ) { dir.create(plotspath)}
         if (!file.exists(datapath) ) { dir.create(datapath)}
         
+        #use tmp2 so you get other breakpoints that were also on the same chromosome
+        tmp4 <- filter(breaks,gene==i)
+        bed <- tmp4 %>% filter(seqnames==chr & filenames %in% tmp3$filenames) %>% select(seqnames,start,end,filenames)
+        bedpath=paste0(plotspath,"breakpoints.bed")
+        export(bed,bedpath,format = "gff3")
         
         readsFiles<- tools::file_path_sans_ext(levels(tmp3$filenames))
-        files2transfer=paste0("../data/browserfiles/",readsFiles,"_reads.bed.gz")
+        files2transfer=paste0("DATA/browserfiles/",readsFiles,"_reads.bed.gz")
         
         file.copy(files2transfer,datapath)
         
-        files2plot=paste0("../data/data/",levels(tmp3$filenames))
+        files2plot=paste0("DATA/rSdata/",levels(tmp3$filenames))
         plotBreakpointsPerChr(files2plot,plotspath = plotspath,chromosomes = c(chr))
       }
     }
   }
 }
+
+stopTimedMessage(ptm)
+
+
+
+ptm <- startTimedMessage()
+
+coresToUse = detectCores()/2
+cl =makeCluster(coresToUse)
+doParallel::registerDoParallel(cl)
+foreach::foreach(i=levels(recurring$gene),.packages = c("rtracklayer","tidyverse","GenomicRanges","breakpointR"),.export = c("repairER")) %dopar% {repairER(i,recurring,breaks)}
+stopCluster(cl)
+
+stopTimedMessage(ptm)
+
 
 
